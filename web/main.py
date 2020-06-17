eabfb4ad8c20d2c02de0fd7d40e1c4ea4062bc24
yuya okada <yuya.okada527@gmail.com>
2020-06-18 01:44:01 +0900

    remove TODO comment

--- web/main.py
+++ web/main.py
@@ -23,9 +23,6 @@ def create():
     # ファイルコンテンツ
     response.data = request.args.get("string")
 
-    # TODO 動作確認
-    print(request.args.get("string"))
-
     # ファイル名をセット
     file_name = "sample.txt"
     response.headers["Content-Disposition"] = f"attachment; filename={file_name}"
