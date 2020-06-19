773935d2ae5946104205a68278c7d85b26cf168f
yuya okada <yuya.okada527@gmail.com>
2020-06-19 19:09:58 +0900

    add config file

---
+++ web/config.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+ENV_FILE = os.path.join(
+    Path(__file__).resolve().parents[1],
+    ".env"
+)
+
+
+class __Settings:
+
+    def __init__(self, path=ENV_FILE):
+        # 指定パスに.envファイルが存在する場合読み込む
+        if os.path.exists(path):
+            load_dotenv(path, verbose=True)
+
+        # 環境変数をセット
+        self.env = os.getenv("ENV", "local")
+
+
+settings = __Settings()
