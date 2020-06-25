61f38984b4abaf193e109c34f2ca728d77b2d507
yuya okada <yuya.okada527@gmail.com>
2020-06-26 00:35:26 +0900

    add enums for font size

--- web/enums.py
+++ web/enums.py
@@ -3,12 +3,26 @@ from enum import Enum
 
 class Env(Enum):
     LOCAL = "local"
-    DEV  = "dev"
-    PROD = "prod"
+    DEV   = "dev"
+    PROD  = "prod"
 
     @classmethod
-    def value_of(cls, value):
+    def name_of(cls, name):
         for e in cls:
-            if e.value == value:
+            if e.value == name:
                 return e
-        raise ValueError(f"{value} is not env name.")
+        raise ValueError(f"{name} is not env name.")
+
+
+class FontSize(Enum):
+    SMALL  = {"name": "small",  "pixel": 50}
+    MIDDLE = {"name": "middle", "pixel": 60}
+    LARGE  = {"name": "large",  "pixel": 80}
+
+    @classmethod
+    def name_of(cls, name):
+        for e in cls:
+            if e.value["name"] == name:
+                return e
+        raise ValueError(f"{name} is not font name.")
+
