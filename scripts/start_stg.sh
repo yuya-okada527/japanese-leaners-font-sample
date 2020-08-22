4a77aa9dd1c8b8087aa2f5eef0a5a0d6403b7ae6
yuya okada <yuya.okada527@gmail.com>
2020-08-22 13:04:10 +0900

    modify stg app name

--- scripts/start_stg.sh
+++ scripts/start_stg.sh
@@ -10,9 +10,9 @@ fi
 docker stop jcw-demo-app
 
 # Dockerビルド
-docker build -t jcw-demo:0.0.1 .
+docker build -t jcw-demo-stg:0.0.1 .
 
 # アプリ起動
 docker run --name jcw-demo-app --rm -d -p 80:5000 \
 --env-file .env \
-jcw-demo:0.0.1
+jcw-demo-stg:0.0.1
