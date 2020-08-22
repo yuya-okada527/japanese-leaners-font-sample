0d4355c6cec69f7f4feb2f6b4f4aa050aa71f562
yuya okada <yuya.okada527@gmail.com>
2020-08-22 13:02:20 +0900

    add link and stg app

---
+++ scripts/start_stg.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# envファイルが存在するか確認
+if [ ! -e .env ]; then
+  echo ".env does not exist."
+  exit 1
+fi
+
+# 起動ずみのコンテナを停止
+docker stop jcw-demo-app
+
+# Dockerビルド
+docker build -t jcw-demo:0.0.1 .
+
+# アプリ起動
+docker run --name jcw-demo-app --rm -d -p 80:5000 \
+--env-file .env \
+jcw-demo:0.0.1
